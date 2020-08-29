f4d2e452e8af04b0a45b5bd2a0ee63fc4b7e5e20
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-29 01:06:41 +0100

    put release_questions on internal.R

--- R/internal.R
+++ R/internal.R
@@ -65,7 +65,17 @@ lmbeta <- function(x){
   sum(lgamma(x)) - lgamma(sum(x))
 }
 
+# nocov start
 
+release_questions <- function(){
+
+  c(
+    "Have you updated the version number in inst/CITATION (two fields)?",
+    "Have you run all the tests listed on the cran-comments.md?"
+  )
+}
+
+# nocov end
 
 
 
